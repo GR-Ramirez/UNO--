c3103b267b7f6db65cd95f46f8286a02abd08cfb
Guillermo Ramirez Bonilla <boss18_3@hotmail.com>
2011-05-16 04:15:20 -0600

    me doy, son las 4 de la madrugada y sigo sin saber que rayos me causa el error.

--- src/CodigoJuego/Jugador.java
+++ src/CodigoJuego/Jugador.java
@@ -1,6 +1,5 @@
 package CodigoJuego;
 
-import CodigoJuego.Baraja;
 import java.util.ArrayList;
 import java.util.Collections;
 /**
@@ -8,35 +7,67 @@ import java.util.Collections;
  * @author Owner
  */
 public class Jugador {
-    final private String nombre;
+    Carta[] wey= {};
+    final public String nombre;
     private long puntos= 0;
     private ArrayList<Carta> mano;
     private byte turnosAPasar= 0;
     Carta tomada;
+    static boolean won;
     
     public Jugador(String nombre){
         this.nombre=nombre;
         this.mano= new ArrayList(Collections.EMPTY_LIST);
     }
-    
+
+    public boolean puede(){
+        if(turnosAPasar== 0)
+            return true;
+        turnosAPasar--;
+        return false;
+    }
     private void tomarCartas(int cuantas, int van){
         if (cuantas==van)
             return;
-        this.mano.add(Baraja.obtenerCarta());
+       won= this.mano.add(Baraja.obtenerCarta());
         tomarCartas(cuantas, ++van);
     }
 
     public void tomarCartas(int meta){
           tomarCartas(meta,0);
+          mano.trimToSize();
     }
 
     public Carta[] exponer(){
         mano.trimToSize();
         Object falsa[]= mano.toArray();
-        return (Carta[])falsa;//conversion tecnicamente innecesaria, pero sirve para documentacion
-    }
+        if(falsa instanceof Carta[])
+        return (Carta[])falsa;
+        return wey;
+   }
 
     void pasar(int turnos){
         this.turnosAPasar += turnos;
+    }
+
+    public void tomarUna(){
+        tomada= Baraja.obtenerCarta();
+    }
+
+    public void ponerEnMano(){
+        won= mano.add(tomada);
+        mano.trimToSize();
+    }
+
+    public void remover(int numero){
+        mano.remove(numero);
+    }
+    
+    int puntuacion(){
+        int punt= 0;
+        for(Carta enMano: mano){
+            punt+= enMano.valor;
+        }
+        return punt;
     }
 }
